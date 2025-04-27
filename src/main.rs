acdb48aaae384f4398b5ebab5d77e0f3b1ecb851
jonathansamsing <jonathan.samsing@boostplm.com>
2025-04-28 00:08:07 +0200

    added history of conversation so that you can resume at any time

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,86 @@
 mod models;
 
-use std::io::stdin;
 use models::{LlmRequest, LlmResponse, Message};
-use reqwest::{blocking::{Client, Response}, header::{HeaderMap, HeaderValue}};
+use reqwest::{
+    blocking::{Client, Response},
+    header::{HeaderMap, HeaderValue},
+};
+use std::{
+    fs::File, io::{stdin, Write}, path::Path
+};
 
 const API_KEY: &'static str = env!("MISTRAL_API_KEY");
-// const DATA: &'static str = include_str!("../data/sample.json");
+const COMPLETIONS_URL: &'static str = "https://api.mistral.ai/v1/chat/completions";
 
+/// Main loop that makes headers, http client, listens for input on stdin, etc.
 fn main() {
+    // ---- LOAD HISTORY ----
+    let history_path = String::from(env!("HOME").to_string() + "/.mistral_cli_chat_history");
     let mut messages: Vec<Message> = vec![];
+
+    if Path::new(&history_path).exists() {
+        let history = std::fs::read_to_string(&history_path).unwrap();
+        messages = serde_json::from_str(&history).unwrap();
+    } else {
+        let mut file = File::create(&history_path).unwrap();
+        file.write_all(b"[]").unwrap();
+    }
+
+    // ---- HEADERS ----
     let mut headers = HeaderMap::new();
-    headers.insert("Authorization", HeaderValue::from_str(&("Bearer ".to_string() + API_KEY)).unwrap());
-    headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
+    headers.insert(
+        "Authorization",
+        HeaderValue::from_str(&("Bearer ".to_string() + API_KEY)).unwrap(),
+    );
+    headers.insert(
+        "Content-Type",
+        HeaderValue::from_str("application/json").unwrap(),
+    );
     headers.insert("Accept", HeaderValue::from_str("application/json").unwrap());
 
+    // ---- MAIN LOOP ----
     loop {
         let mut input = String::new();
         stdin().read_line(&mut input).expect("Failed to read line");
-        messages.push(Message { role: String::from("user"), content: input.clone() });
+
+        if input == "quit\n".to_string() {
+            println!("Writing history to disk and quitting...");
+            break;
+        }
+
+        messages.push(Message {
+            role: String::from("user"),
+            content: input.clone(),
+        });
 
         let llm_req = LlmRequest::from_messages(&messages);
         let mes = serde_json::to_string(&llm_req).expect("Couldn't serialize request");
 
-        let _ = headers.insert("Content-Length", HeaderValue::from_str(stringify!(len(mes))).unwrap());
+        let _ = headers.insert(
+            "Content-Length",
+            HeaderValue::from_str(stringify!(len(mes))).unwrap(),
+        );
 
-        let client = Client::builder().default_headers(headers.clone()).build().unwrap();
-        let req = client.post("https://api.mistral.ai/v1/chat/completions").body(mes);
+        let client = Client::builder()
+            .default_headers(headers.clone())
+            .build()
+            .unwrap();
+        let req = client
+            .post(COMPLETIONS_URL)
+            .body(mes);
 
         match req.send() {
             Ok(v) => {
-                println!("Sent request.");
-                if let Err(e) = handle_received(v, &mut messages) {
-                    println!("Unsuccessfully handled response.\n  {:?}", e);
-                    let res2 = client.post("https://api.mistral.ai/v1/chat/completions");
-                    println!("Attempt to print bytes:\n  {:?}", res2.send().unwrap().bytes());
+                match handle_received(v) {
+                    Err(e) => {
+                        println!("Unsuccessfully handled response.\n  {:?}", e);
+                        let res2 = client.post(COMPLETIONS_URL);
+                        println!(
+                            "Attempt to print bytes:\n  {:?}",
+                            res2.send().unwrap().bytes()
+                        );
+                    }
+                    Ok(new_messages) => messages.extend(new_messages.clone()),
                 }
             }
             Err(e) => {
@@ -41,13 +88,21 @@ fn main() {
             }
         }
     }
+
+    std::fs::write(history_path, serde_json::to_string(&messages).unwrap()).unwrap();
 }
 
-fn handle_received(r: Response, messages: &mut Vec<Message>) -> reqwest::Result<()> {
+/// Upon receiving a response via HTTP, this method is used to parse it into the models
+/// specified in src/models.rs and print it to the terminal.
+fn handle_received(r: Response) -> reqwest::Result<Vec<Message>> {
     let response: LlmResponse = r.json()?;
     for choice in &response.choices {
         println!("{}", choice.message.content);
     }
-    messages.extend(response.choices.into_iter().map(|c| c.message.to_message()));
-    Ok(())
+    let new_messages: Vec<Message> = response
+        .choices
+        .into_iter()
+        .map(|c| c.message.to_message())
+        .collect();
+    Ok(new_messages)
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+/// The "top-level" of the API's response contains these fields.
 #[derive(Deserialize, Debug)]
 pub struct LlmResponse {
     id: String,
@@ -10,6 +11,7 @@ pub struct LlmResponse {
     usage: LlmUsage,
 }
 
+/// No idea what this means, but contains the actual content of the response inside `message`.
 #[derive(Deserialize, Debug)]
 pub struct LlmChoice {
     index: usize,
@@ -17,6 +19,7 @@ pub struct LlmChoice {
     finish_reason: String,
 }
 
+/// How much the LLM spent on different scheiße.
 #[derive(Deserialize, Debug)]
 struct LlmUsage {
     prompt_tokens: u32,
@@ -24,6 +27,7 @@ struct LlmUsage {
     completion_tokens: u32,
 }
 
+/// Contents of the message, etc.
 #[derive(Deserialize, Serialize, Debug)]
 pub struct LlmMessage {
     role: String,
@@ -32,6 +36,8 @@ pub struct LlmMessage {
 }
 
 impl LlmMessage {
+    /// Convenience method that just removes the `tool_calls` field so that you can push it to a
+    /// vec of `Message`.
     pub fn to_message(self) -> Message {
         Message {
             role: self.role,
@@ -40,12 +46,14 @@ impl LlmMessage {
     }
 }
 
-#[derive(Serialize, Debug, Clone)]
+/// Is used for sending a message, as well as storing the message history.
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Message {
     pub role: String,
     pub content: String,
 }
 
+/// The "top-level" of the http request body that we send to the Mistral API.
 #[derive(Serialize, Debug)]
 pub struct LlmRequest {
     model: LlmModel,
@@ -53,18 +61,19 @@ pub struct LlmRequest {
 }
 
 impl LlmRequest {
+    /// When input is read from stdin
     pub fn from_str(input: &str) -> Self {
         LlmRequest {
             model: LlmModel::MistralLargeLatest,
-            messages: vec![
-                Message {
-                    role: "user".to_string(),
-                    content: input.to_string(),
-                }
-            ],
+            messages: vec![Message {
+                role: "user".to_string(),
+                content: input.to_string(),
+            }],
         }
     }
 
+    /// This is used after pushing the latest message to `messages`, and simply wraps the messages
+    /// to add info about the model type as well.
     pub fn from_messages(messages: &Vec<Message>) -> Self {
         LlmRequest {
             model: LlmModel::MistralLargeLatest,
@@ -73,6 +82,7 @@ impl LlmRequest {
     }
 }
 
+/// Availabe model types from the Mistral API. (Obviously incomplete you dummy)
 #[derive(Serialize, Debug)]
 pub enum LlmModel {
     #[serde(rename = "mistral-large-latest")]
@@ -80,11 +90,10 @@ pub enum LlmModel {
 }
 
 impl LlmModel {
+    /// Unused since I just tag the field with a serde rename instead LMAO
     pub fn as_str(&self) -> &'static str {
         match self {
-            LlmModel::MistralLargeLatest => {
-                "mistral-large-latest"
-            }
+            LlmModel::MistralLargeLatest => "mistral-large-latest",
         }
     }
 }
